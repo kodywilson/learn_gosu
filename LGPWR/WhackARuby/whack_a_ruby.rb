662297ca8de826ede0ab12ab0fcafb801d1d5faa
Kody Wilson <kodywilson@gmail.com>
2021-04-27 15:30:01 -0700

    add time constraint

--- LGPWR/WhackARuby/whack_a_ruby.rb
+++ LGPWR/WhackARuby/whack_a_ruby.rb
@@ -16,27 +16,34 @@ class WhackARuby < Gosu::Window
     @hit = 0
     @font = Gosu::Font.new(30)
     @score = 0
+    @playing = true
   end
 
   def button_down(id)
-    if (id == Gosu::MsLeft)
-      if Gosu.distance(mouse_x, mouse_y, @x, @y) < 50 && @visible >= 0
-        @hit = 1
-        @score += 5
-      else
-        @hit = -1
-        @score -= 1
+    if @playing
+      if (id == Gosu::MsLeft)
+        if Gosu.distance(mouse_x, mouse_y, @x, @y) < 50 && @visible >= 0
+          @hit = 1
+          @score += 5
+        else
+          @hit = -1
+          @score -= 1
+        end
       end
     end
   end
 
   def update
-    @x += @velocity_x
-    @y += @velocity_y
-    @velocity_x *= -1 if @x + @width/2 > 800 || @x - @width/2 < 0
-    @velocity_y *= -1 if @y +@height/2 > 600 || @y - @height/2 < 0
-    @visible -= 1
-    @visible = 60 if @visible < -10 && rand < 0.01
+    if @playing
+      @x += @velocity_x
+      @y += @velocity_y
+      @velocity_x *= -1 if @x + @width/2 > 800 || @x - @width/2 < 0
+      @velocity_y *= -1 if @y +@height/2 > 600 || @y - @height/2 < 0
+      @visible -= 1
+      @visible = 60 if @visible < -10 && rand < 0.01
+      @time_left = (60 - (Gosu.milliseconds / 1000))
+      @playing = false if @time_left <= 0
+    end
   end
 
   def draw
@@ -54,6 +61,11 @@ class WhackARuby < Gosu::Window
     draw_quad(0,0,c,800,0,c,800,600,c,0,600,c)
     @hit = 0
     @font.draw_text(@score.to_s, 700, 20, 2)
+    @font.draw_text(@time_left.to_s, 20, 20, 20)
+    unless @playing
+      @font.draw_text('Game Over', 300, 300, 3)
+      @visible = 20
+    end
   end
 end
 
