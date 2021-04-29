cad87cae20e93383fee4af513e630117038ca852
Kody Wilson <kodywilson@gmail.com>
2021-04-28 21:06:39 -0700

    Add acceleration to ship movement

--- LGPWR/SectorFive/player.rb
+++ LGPWR/SectorFive/player.rb
@@ -5,10 +5,20 @@ class Player
     @y = 200
     @angle = 0.0
     @image = Gosu::Image.new('images/ship.png')
+    @velocity_x = 0
+    @velocity_y = 0
   end
 
-  def draw
-    @image.draw_rot(@x, @y, @angle)
+  def accelerate
+    @velocity_x += Gosu.offset_x(@angle, 2)
+    @velocity_y += Gosu.offset_y(@angle, 2)
+  end
+
+  def move
+    @x += @velocity_x
+    @y += @velocity_y
+    @velocity_x *= 0.9
+    @velocity_y *= 0.9
   end
 
   def turn_right
--- LGPWR/SectorFive/sector_five.rb
+++ LGPWR/SectorFive/sector_five.rb
@@ -11,6 +11,8 @@ class SectorFive < Gosu::Window
   def update
     @player.turn_left if button_down?(Gosu::KbLeft)
     @player.turn_right if button_down?(Gosu::KbRight)
+    @player.accelerate if button_down?(Gosu::KbUp)
+    @player.move
   end
 
   def draw
